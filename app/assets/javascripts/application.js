2361daeddc56dbf5c2505ed41211dc9a0ace2b33
github username <github email address>
2019-05-15 19:00:53 -0700

    scrolling automatically goes down to last msg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,17 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+
+const scroll_bottom = () => {
+  if ($('#messages').length > 0) {
+    return $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 })
